02f10ef813af188b5c7dd587ba3f1dc428ddaafa
Jose Zena Zamora <jose.z@turing.com>
2022-07-03 06:19:05 -0500

    feat: adding apolloClient

--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from 'react';
-import './Explorer.css'
+import { useQuery } from '@apollo/client';
+import {
+  GET_ALL_NOTES,
+  GET_TAGS_AND_ENTITIES,
+} from '../../services/Apollo/Queries';
+import './Explorer.css';
 export type ExplorerProps = {
-  value: string
-  placeholder: string
-  onchange: any
-  onKeyPress: any
-  onFocus: any
-}
+  value: string;
+  placeholder: string;
+  onchange: any;
+  onKeyPress: any;
+  onFocus: any;
+};
 
 export const Explorer: FC<ExplorerProps> = () => {
-  return <div className='explorer'>explorer test from github</div>;
+  const { data: notesData } = useQuery(GET_ALL_NOTES);
+  const { data: tagAndEntitiesData } = useQuery(GET_TAGS_AND_ENTITIES);
+  console.log('notesData::', notesData);
+  console.log('tagAndEntitiesData::', tagAndEntitiesData);
+  return <div className="explorer">explorer test from github</div>;
 };
 
 export default Explorer;
+
