d4df12873210263f60cf5ceae3f0de866df7a0b6
Jose Zena Zamora <jose.z@turing.com>
2022-07-06 09:06:02 -0500

    feat: adding mutations

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
+// components
 export * from './components/SearchOS'
+
+
+// schemas
+import * as typeDefinitions from './services/Apollo/typedefs'
+import * as queries from './services/Apollo/Queries'
+import * as mutations from './services/Apollo/Mutations'
+
+export {
+  typeDefinitions,
+  queries,
+  mutations
+}
