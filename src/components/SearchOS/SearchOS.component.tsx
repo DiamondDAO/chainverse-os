bdf2c084b904eeba2440bee59b545021855c6867
Jose Zena Zamora <jose.z@turing.com>
2022-07-07 11:22:08 -0500

    feat: adding spinner

--- src/components/SearchOS/SearchOS.component.tsx
+++ src/components/SearchOS/SearchOS.component.tsx
@@ -16,13 +16,16 @@ import {
   useGetTagsTableData,
 } from '../../hooks/search';
 import { SearchOSProps } from './SearchOS';
+import Spinner from '../Spinner/Spinner';
 export enum SearchTypes {
   Blocks = 'blocks',
   Tags = 'tags',
   Entities = 'entities',
 }
 
-const SearchComponent: FC<SearchOSProps> = (props: SearchOSProps) => {
+const SearchComponent: FC<SearchOSProps> = (
+  props: SearchOSProps
+): JSX.Element => {
   // graphql
   const { data: notesData, loading: loadingNotes } = useQuery(GET_ALL_CREATED);
   const { data: tagAndEntitiesData, loading: loadingEntityTag } = useQuery(
@@ -36,6 +39,7 @@ const SearchComponent: FC<SearchOSProps> = (props: SearchOSProps) => {
   // search state
   const [term, setTerm] = useState('');
   const [searchType, setSearchType] = useState(SearchTypes.Entities);
+  const [showTypesFilter, setShowTypeFilter] = useState(false);
   const isEntityFilter = searchType === SearchTypes.Entities;
   const isTagFilter = searchType === SearchTypes.Tags;
   const isBlockFilter = searchType === SearchTypes.Blocks;
@@ -123,11 +127,15 @@ const SearchComponent: FC<SearchOSProps> = (props: SearchOSProps) => {
   };
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     //TODO: apply debounce
-    setTerm(e.target.value);
-    props.onChange(e.target.value);
+    const value = e.target.value;    
+    setTerm(value);
+    props.onChange(value);
   };
   const handleOnKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
+      if (term.length > 0) {
+        setShowTypeFilter(true);
+      }
       props.onEnter({
         searchType,
         entity: entityResponse,
@@ -143,11 +151,11 @@ const SearchComponent: FC<SearchOSProps> = (props: SearchOSProps) => {
   const classActive = 'bg-[#95438d] text-white';
   return (
     <div className="flex bg-white flex justify-center items-center space-x-2">
-      <div className="flex border rounded items-center px-2">
+      <div className="flex border rounded items-center px-2 shadow">
         {isLoading ? (
-          <div className="text-sm text-gray">loading...</div>
+          <Spinner color="darkBlue"></Spinner>
         ) : (
-          <SearchIcon className="w-5 h-5" />
+          <SearchIcon className="w-5 h-5 mr-3" />
         )}
         <input
           type="text"
@@ -158,32 +166,34 @@ const SearchComponent: FC<SearchOSProps> = (props: SearchOSProps) => {
           placeholder="Start with a search for any keyword, community name, or user"
         />
       </div>
-      <div className="flex items-center space-x-2">
-        <span
-          className={`p-2 border rounded cursor-pointer ${
-            isEntityFilter ? classActive : ''
-          }`}
-          onClick={() => handleOnChangeType(SearchTypes.Entities)}
-        >
-          Entities ({entityFuseSearchResult.length})
-        </span>
-        <span
-          className={`p-2 border rounded cursor-pointer ${
-            isBlockFilter ? classActive : ''
-          }`}
-          onClick={() => handleOnChangeType(SearchTypes.Blocks)}
-        >
-          Blocks ({blocksFuseSearchResult.length})
-        </span>
-        <span
-          className={`p-2 border rounded cursor-pointer ${
-            isTagFilter ? classActive : ''
-          }`}
-          onClick={() => handleOnChangeType(SearchTypes.Tags)}
-        >
-          Tags ({tagFusSearchResult.length})
-        </span>
-      </div>
+      {showTypesFilter && (
+        <div className="flex items-center space-x-2">
+          <span
+            className={`p-2 border rounded cursor-pointer ${
+              isEntityFilter ? classActive : ''
+            }`}
+            onClick={() => handleOnChangeType(SearchTypes.Entities)}
+          >
+            Entities ({entityFuseSearchResult.length})
+          </span>
+          <span
+            className={`p-2 border rounded cursor-pointer ${
+              isBlockFilter ? classActive : ''
+            }`}
+            onClick={() => handleOnChangeType(SearchTypes.Blocks)}
+          >
+            Blocks ({blocksFuseSearchResult.length})
+          </span>
+          <span
+            className={`p-2 border rounded cursor-pointer ${
+              isTagFilter ? classActive : ''
+            }`}
+            onClick={() => handleOnChangeType(SearchTypes.Tags)}
+          >
+            Tags ({tagFusSearchResult.length})
+          </span>
+        </div>
+      )}
     </div>
   );
 };
